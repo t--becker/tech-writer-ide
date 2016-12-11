3a4e712b150fbdd6070f4d1ae870ff5e1a706d75
Tony Becker <jab9becker@yahoo.com>
2016-12-11 15:51:37 +1100

    First Commit

---
+++ spec/tiger-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TigerPackageView from '../lib/tiger-package-view';
+
+describe('TigerPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
